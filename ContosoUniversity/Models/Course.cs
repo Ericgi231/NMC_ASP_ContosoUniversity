5e86f002e7998eb123c62014235a3e9018fb6774
grant121 <grant121@JB202-L17-W-D>
2019-02-26 13:07:26 -0500

    About to add phone number

--- ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/Models/Course.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
@@ -10,8 +11,15 @@ namespace ContosoUniversity.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int CourseID { get; set; }
+        [Required]
+        [ConcurrencyCheck]
+        [MaxLength(24, ErrorMessage = "The maximum length is 24 characters")]
+        [MinLength(5, ErrorMessage = "The minimum length is 5 characters")]
+        [Index(IsUnique = true)]
         public string Title { get; set; }
+        [Range(1,6)]
         public int Credits { get; set; }
+
 
         public ICollection<Enrollment> Enrollments { get; set; }
     }
